673ffd3b52ef605276dbc9e4b5fb2ea60f7128cb
longtn <long.tn3101@gmail.com>
2023-09-15 21:49:33 +0700

    j

---
+++ src/main/java/Image.java
@@ -0,0 +1,8 @@
+
+public class Image {
+	String name;
+	String tag;
+	String url;
+	String title;
+	
+}
---
+++ src/main/java/MAIN.java
@@ -0,0 +1,10 @@
+
+public class MAIN {
+	public static void main(String[] args) {
+			String b="https://r4.wallpaperflare.com/wallpaper/410/867/750/vector-forest-sunset-forest-sunset-forest-wallpaper-b3abc35d0d699b056fa6b247589b18a8.jpg";
+			int index=b.lastIndexOf("/");
+			String c= b.substring(index + 1);
+			System.out.println(c);
+			
+	}
+}
